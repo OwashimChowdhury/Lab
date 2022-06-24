8e60019a294dc10ee503d6e03b38f26ee102d8a1
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-24 11:17:59 +0530

    Update Mul.java

--- Mul.java
+++ Mul.java
@@ -11,7 +11,7 @@ public static void main(String[]args){
 int a=50; //using int data type and variable name is a
 int b=10; //using int data type and variable name is b
 
-System.out.print("multiplivcation of two number is:");
+System.out.print("multiplivcation of two number is:"); //printing multiplivcation of two number is:
 System.out.println(a*b); //Multiplying two numbers
 }
 }
