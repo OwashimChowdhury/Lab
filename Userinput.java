43f15068d32adb2bd6fbe3b1eed3505c6bd712ee
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-24 11:20:15 +0530

    Update Userinput.java

--- Userinput.java
+++ Userinput.java
@@ -10,7 +10,7 @@ class Userinput{ //created a class Userinput
 
   public static void main(String[] args) {
     Scanner a = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter your name:");
+    System.out.println("Enter your name:");  //printing Enter your name:
 
     String Name = a.nextLine();  // Read 
     System.out.println("Username is: " + Name);  // Output 
