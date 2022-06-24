2fce2b91a35b9a9c52ec8430ba8e24b8216a0af2
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-24 11:02:04 +0530

    Update Userinput.java

--- Userinput.java
+++ Userinput.java
@@ -1,12 +1,18 @@
+/*
+@author: Owashim Chowdhury
+ 
+*/
+//java program to take input from user
+
 import java.util.Scanner;  // Import the Scanner class
 
-class Userinput{
+class Userinput{ //created a class Userinput
 
   public static void main(String[] args) {
     Scanner a = new Scanner(System.in);  // Create a Scanner object
     System.out.println("Enter your name:");
 
-    String Name = a.nextLine();  // Read user input
-    System.out.println("Username is: " + Name);  // Output user input
+    String Name = a.nextLine();  // Read 
+    System.out.println("Username is: " + Name);  // Output 
   }
 }
