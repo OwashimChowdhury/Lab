1b1c43add2a532b1eedc5bff374262c8f0a7331e
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-24 11:12:24 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -11,7 +11,7 @@ public static void main(String[]args){
  int a=50; //using int data type and variable name is a
  int b=10; //using int data type and variable name is b
 
-System.out.print("addituion of two number is:");
+System.out.print("addituion of two number is:"); //printing addituion of two number is:
 System.out.println(a+b); //adding two numbers
 }
 }
