09c418239ba240a8b6183616024ef8afc7ea07b1
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-30 14:11:57 +0530

    Update Leapyear.java

--- Leapyear.java
+++ Leapyear.java
@@ -1,3 +1,7 @@
+/*
+Author:Owashim Chowdhury
+*/
+//java program to check leap year or not
 package Owas;
 
 import java.util.Scanner;
@@ -6,17 +10,17 @@ public class Leapyear {
 
 	public static void main(String[] args) {
 		 
-		Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);  
 		 for(int i=1;i>=0;i++) {
-		System.out.println("enter Year:");
+		System.out.println("enter Year:");  //taking input
 		  int year =sc.nextInt();
 		 
-if (year%4==0){
+if (year%4==0){ //logic
 	System.out.println("yes, this is leap year");
      }
 else{
 	System.out.println("no,this is not leap year");
     }
+}		//end of for loop
 }
-}
-}
+}	//end of class
