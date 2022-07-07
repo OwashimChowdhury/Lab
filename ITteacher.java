a2ec72047b6d3c9cc57a1ea9d3f3d2c9871d515e
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-07-07 15:35:34 +0530

    Update ITteacher.java

--- ITteacher.java
+++ ITteacher.java
@@ -1,18 +1,26 @@
+/*
+@author:owashim chowdhury
+Q.2… In this example, you have a base class Teacher and a sub class ITTeacher.
+Since class ITTeacher extends the designation and college properties and work () method from base class,
+we need not to declare these properties and method in sub class.
+Here we have college Name, designation and work () method which are common to all the teachers so we have declared them in the base class, 
+this way the child classes like Math Teacher, Music Teacher and PhysicsTeacher do not need to write this code and can be used directly from base class.
+*/
 package Lab;
 
-class Teacher{
+class Teacher{	//parent class
 	String designation="designation is teacher,";
 	String collegename=" college is IMPS";
 	void work() {
 		System.out.println("work is Teaching");
 	}
 }
- class ITteacher extends Teacher {
+ class ITteacher extends Teacher {	//child class
 
 	public static void main(String[] args) {
 		ITteacher te=new ITteacher();
 		System.out.println(te.designation+" "+te.collegename);
-		te.work();
-	}
+		te.work();	//calling a  parent class method
+	}	//end of main method
 
-}
+}	//end of class
