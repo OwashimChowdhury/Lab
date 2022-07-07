62bd0734361e02116fbd1f2d89fce3e2f815d945
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-07-07 15:24:48 +0530

    Update Adder.java

--- Adder.java
+++ Adder.java
@@ -1,7 +1,13 @@
+/*
+Author:Owashim Chowdhury
+Q.1… Write the following code:
+	a)A class named Arithmetic with a method named add that takes integers as parameters and returns an integer denoting their sum.
+	b)A class named Adder that inherits from a superclass named Arithmetic
+*/
 
 package Lab;
 
-class Arithmatic{
+class Arithmatic{	//super class
 	 int a;
 	 int b;
 	 int sum;
@@ -10,14 +16,13 @@ class Arithmatic{
 	}
 	
 }
- class Adder extends Arithmatic {
+ class Adder extends Arithmatic {	//sub class
 	 void display() {	//method 2
 			System.out.println(sum);
 		}
 	public static void main(String[] args) {
-		Adder ac=new Adder();
-		ac.add(55, 66);
-		ac.display();
-	}
-
-}
+		Adder ac=new Adder();	//object of sub class
+		ac.add(55, 66);	//calling method of super class
+		ac.display();	//calling method of sub class
+	}	//end of main method
+}	//end of class
