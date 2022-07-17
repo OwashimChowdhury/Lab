c7ad3b4d6fd0617e35ddea2084decc8fc1984afb
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-07-17 23:38:33 +0530

    Update MyCalculator1.java

--- MyCalculator1.java
+++ MyCalculator1.java
@@ -11,13 +11,13 @@ interface AdvancedArithmetic{
 	}
 //class implements interface
 	class MyCalculator implements AdvancedArithmetic {
-	    public int divisor_sum(int n) {
+	    public int divisor_sum(int n) {	//method 1
 	        int sum=0;
 	        for(int i=1;i<=n;i++) {
-	            if(n%i==0)
-	                sum+=i;
+	            if(n%i==0)	//logic
+	                sum+=i;	//sum of divisors
 	        }
-	        return sum;
+	        return sum;	//returning sum
 	    }
 	}
 	
@@ -26,11 +26,11 @@ public class MyCalculator1 {
 	public static void main(String[] args) {
 	MyCalculator m=new MyCalculator();	//creating object of myCalculator class
 	Scanner sc=new Scanner(System.in);
-	System.out.println("enter a number:");
+	System.out.println("enter a number:");	//taking input
 	int n=sc.nextInt();
 	sc.close();
 	System.out.println("I implemented: AdvancedArithmetic:");
-	System.out.println("sum of the divisor is :"+m.divisor_sum(n));
+	System.out.println("sum of the divisor is :"+m.divisor_sum(n));	//output
 
 	}
 
