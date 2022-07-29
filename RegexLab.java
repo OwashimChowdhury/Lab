acecc4adc08f4b776cb1113ed22f855ee0349cba
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-07-29 10:14:17 +0530

    Update RegexLab.java

--- RegexLab.java
+++ RegexLab.java
@@ -8,10 +8,10 @@ import java.util.regex.Pattern;
 
 class MyRegex {
     String regex = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";//logic
-    String pattern = regex + "." +  regex + "." +  regex + "." + regex;
+    String pattern = regex + "." +  regex + "." +  regex + "." + regex;//pattern to match
     
        void check (String s ) {//method to check the pattern match or not
-    	boolean b=Pattern.matches( pattern, s);
+    	boolean b=Pattern.matches( pattern, s);//check the pattern match or not
     	System.out.println(b);
     }
 }
@@ -21,11 +21,11 @@ public class RegexLab {
 	public static void main(String[] args) {//main method
 		MyRegex m=new MyRegex();
 		m.check("000.12.12.034");//calling check method
-		m.check("121.234.12.12");
-		m.check("23.45.12.56");
-		m.check("00.12.123.123123.123");
-		m.check("122.23");
-		m.check("Hello.IP");
+		m.check("121.234.12.12");//calling check method
+		m.check("23.45.12.56");//calling check method
+		m.check("00.12.123.123123.123");//calling check method
+		m.check("122.23");//calling check method
+		m.check("Hello.IP");//calling check method
 
 	}
 
