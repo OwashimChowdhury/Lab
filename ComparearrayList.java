e921632cc87c2f7e673d96a540eafe74075f1e83
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-08-12 15:27:04 +0530

    Update ComparearrayList.java

--- ComparearrayList.java
+++ ComparearrayList.java
@@ -14,14 +14,14 @@ public class ComparearrayList {
 		arr.add("orange");
 		arr.add("white");
 		arr.add("black");
-		//creating another array
+		//creating another array list
 		ArrayList<String> arr1=new ArrayList<String>();
 		arr1.add("red");
 		arr1.add("green");
 		arr1.add("orange");
 		arr1.add("black");
 		
-		//creating another array 
+		//creating another array list
 		 ArrayList<String> arr2 = new ArrayList<String>();
          for (String e : arr)//for each loop
             arr2.add(arr1.contains(e) ? "Yes" : "No");//checking all the elements matched or not
