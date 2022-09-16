9b02b417c0d943994cac2400cb2c03f8681bc8b9
Owashim Chowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-09-16 14:17:31 +0530

    Update App.java

--- hibernatedemo/src/main/java/com/hibernate/hibernatedemo/App.java
+++ hibernatedemo/src/main/java/com/hibernate/hibernatedemo/App.java
@@ -8,19 +8,26 @@ public class App
 {
 	public static void main( String[] args )
     {
+	//creating object of StudentDaoImpl class
        StudentDaoImpl dao = new StudentDaoImpl();
        char c;
+		//creating do while loop
        do{
+	       //creating scanner class object
     	   Scanner sc = new Scanner(System.in);
     	   System.out.println("PRESS 1 for add student \nPRESS 2 for display student \nPRESS 3 for update \nPRESS 4 for delete \nPRESS 5 for Exit");
     	   int a = sc.nextInt();
     	   switch(a) {
+	  //calling addstudent method
     	   case 1: dao.addStudent();
     	   break;
+	//calling fetchstudent method
     	   case 2: dao.fetchStudent();
     	   break;
+	//calling updatestudent method	   
     	   case 3: dao.updateStudent();
     	   break;
+	 //calling deletestudent method
     	   case 4: dao.deleteStudent();
     	   break;
     	   case 5: System.exit(0);
