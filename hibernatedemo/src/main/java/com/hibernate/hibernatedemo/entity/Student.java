b520bd78f85ae9c9c8b6b652201e1a75c820d976
Owashim Chowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-09-16 15:39:15 +0530

    Update Student.java

--- hibernatedemo/src/main/java/com/hibernate/hibernatedemo/entity/Student.java
+++ hibernatedemo/src/main/java/com/hibernate/hibernatedemo/entity/Student.java
@@ -14,18 +14,23 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="Student_details")
+@Table(name="Student_details")//table name is Student_details
 public class Student {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)//auto generated id
+	//taking id column
 	private int id;
 	@Column(length = 20, nullable = false)
+	//taking name column
 	private String name;
 	@Column(length = 11, nullable = false, unique = true)
+	//taking phone column
 	private long phone;
 	@Column(length = 20, nullable = false)
+	//taking dept column
 	private String dept;
 	@Column(length = 20, nullable = false, unique = true)
+	//taking email column
 	private String email;
 	
 }
