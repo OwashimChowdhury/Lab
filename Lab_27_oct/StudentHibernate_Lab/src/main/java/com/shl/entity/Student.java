8a71fe783e19845daac98e4a6fd1820416ffe429
Owashim Chowdhury <chowdhuryowashim@gmail.com>
2022-10-27 19:03:39 +0530

    added

---
+++ Lab_27_oct/StudentHibernate_Lab/src/main/java/com/shl/entity/Student.java
@@ -0,0 +1,21 @@
+package com.shl.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int sId;
+	@Column(length = 30,nullable = false)
+	private String sName;
+}
